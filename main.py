4232c894e22aab515d3bce557e0c462c6f6cb7ef
MaykonRobert <101935958+maykonrobert@users.noreply.github.com>
2022-03-22 18:40:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Simulador de compra MR Imóveis
+nome = input("Qual é o seu nome completo: ")
+
+idade = input  ("Qual o seu ano de nascimento: ")
+data_nascimento = 2022 - int(idade)
+
+print(data_nascimento)
+'''
+
+ano_nascimento = input('Em que ano você nasceu? ')
+idade = 2022 - ano_nascimento
+
+print(idade)
+'''
+
+
